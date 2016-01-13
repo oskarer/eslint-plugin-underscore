c74c12fc8e54a18132c39dfd247ede14fc653130
Jordan Eldredge <jeldredge@hearsaycorp.com>
2016-01-13 19:45:12 +0000

    Make isMemberExpOf handle missing node

--- tests/lib/rules/prop-shorthand.js
+++ tests/lib/rules/prop-shorthand.js
@@ -27,6 +27,7 @@ ruleTester.run('prop-shorthand', rule, {
         'var r = _.map([], function() { return React.PropTypes.object; })',
         'var ids = _(arr).map(function (i) { return i.a.b.c; });',
         'var ids = _(arr).map("x").map("y").map(function (i) { return i.a.b; });',
+        'var ids = _.map([], function (i) { var a = b; return i.a; });',
         'var r = _.map([])',
         {
             code: 'var ids = _.map([], i => i.a.b.c);',
