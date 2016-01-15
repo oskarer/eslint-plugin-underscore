8204a4472ebb4cc991aba702fa96d14eb105f7b7
Jordan Eldredge <jordan@jordaneldredge.com>
2016-01-14 17:04:50 -0800

    Revert prefer-pluck back to only handling 'property' case

--- tests/lib/rules/prefer-pluck.js
+++ tests/lib/rules/prefer-pluck.js
@@ -18,42 +18,30 @@ var rule = require("../../../lib/rules/prefer-pluck"),
 //------------------------------------------------------------------------------
 
 var ruleTester = new RuleTester();
-var wrongNameMessage = 'Prefer _.pluck to map when collecting a single property';
-var wrongSyntaxMessage = 'Prefer a "property" string to a function that returns a property' ;
-
-var errors = {
-    property: [{ message: wrongNameMessage }],
-    func: [{ message: wrongNameMessage }, { message: wrongSyntaxMessage }]
-}
+var errors = [{mesage: 'Prefer _.pluck to map when collecting a single property' }];
 
 ruleTester.run("prefer-pluck", rule, {
 
     valid: [
         "var ids = _.pluck(items, 'id');",
-        "var ids = _.map(items, {id: 1});"
+        "var ids = _.map(items, {id: 1});",
+        "var ids = _.map(items, function(item){ return item.id; });",
+        'var ids = _.map([], function (i) { return i.a; });',
     ],
 
     invalid: [
         {
-            code: "var ids = _.map(items, function(item){ return item.id; });",
-            errors: errors.func,
-            output: "var ids = _.pluck(items, 'id');"
-        }, {
             code: "var ids = _.map(items, 'id');",
-            errors: errors.property,
+            errors: errors,
             output: "var ids = _.pluck(items, 'id');"
         }, {
             code: "var ids = _(items).map('id');",
-            errors: errors.property,
+            errors: errors,
             output: "var ids = _(items).pluck('id');"
         }, {
             code: "var ids = _.chain(items).map('id');",
-            errors: errors.property,
+            errors: errors,
             output: "var ids = _.chain(items).pluck('id');"
-        }, {
-            code: 'var ids = _.map([], function (i) { return i.a; });',
-            errors: errors.func,
-            output: "var ids = _.pluck([], 'a');"
         }
     ]
 });
